764489422141eb1b6153511b2eef96d8e99f50cb
Pierre <prrvchr@gmail.com>
2024-01-09 05:21:34 +0100

    new version 3.6.0

--- Fluid-Attacks-Scan.sh
+++ Fluid-Attacks-Scan.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -v ~/github/pyrdfa3:/working-dir ghcr.io/fluidattacks/makes/amd64 m gitlab:fluidattacks/universe@trunk /skims scan ./_fascan.yml
+docker run -v ~/github/pyrdfa3/source/pyrdfa3:/working-dir ghcr.io/fluidattacks/makes/amd64 m gitlab:fluidattacks/universe@trunk /skims scan ./_fascan.yml
---
+++ generateDoc.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+pdoc -o doc ./src/pyRdfa !./src/pyRdfa/extras ./src/pyRdfaExtras !./src/pyRdfaExtras/serializers
